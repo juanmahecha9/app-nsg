417beeec51d123cbe3cd8f271c78531ca2e1c02a
Juan David <user@Juans-iMac.local>
2020-07-11 00:45:21 -0500

    limitador de vidas

--- NSG-APP/src/assets/js/tablero.js
+++ NSG-APP/src/assets/js/tablero.js
@@ -1,227 +1,256 @@
 function egipto(){
+
+  var contador = 100;
+  function con(c){
+
+    if(c < 97){
+      document.getElementById("ms").textContent = "El virus te ha derrotado, ya es inútil buscar la cura";
+    } 
+  }
+
   let pastGuesses = [];
   let numberOfHits = 0;
   let numberOfMisses = 0;
   let loggedMessageTimes = [];
 
-  class GameBoard {
-    constructor(cellNumber) {
-      this.cellNumber = [];
 
-      this.checkAvailableSpace = function (
-        isVertical,
-        startNumber,
-        shipLength
-      ) {
-        const freeSpaces = [];
-        let startCell = startNumber;
-        let endCell = 0;
-        let increase = 1;
-
-        isVertical
-          ? ((endCell = startNumber + 91), (increase = 10))
-          : ((startCell = startNumber * 10), (endCell = startCell + 10));
-
-        for (let i = startCell; i < endCell; i += increase) {
-          if (typeof this[i] === "undefined") {
-            freeSpaces.push(i);
-          } else {
-            freeSpaces.push("");
-          }
-        }
-        return findPossiblePoints(freeSpaces, shipLength);
-      };
-
-      this.placeShip = function (ship) {
-        const isVertical = randomNumber(1);
-        const increase = isVertical * 9 + 1;
-        const endCell = ship.health * increase;
-        let keepGoing = true;
-
-        while (keepGoing) {
-          let startIndex = randomNumber(9);
-          var possibilities = this.checkAvailableSpace(
-            isVertical,
-            startIndex,
-            ship.health
-          );
-          possibilities.length == 0
-            ? (keepGoing = true)
-            : (keepGoing = false);
-        }
-        const shipStart = chooseRandomFromArray(possibilities);
-        console.log(possibilities);
-        console.log("placing " + ship.name + " at " + shipStart);
-
-        for (let i = 0; i < endCell; i += increase) {
-          gameBoard[shipStart + i] = ship;
-        }
-      };
-
-      this.checkIfShotHitsAnything = (newGuess) => {
-        this[parseInt(newGuess)]
-          ? (hit(newGuess), this[newGuess].resultOfGettingHit())
-          : miss(newGuess); // calls ship.resultOfGettingHit() or tells "missed"
-      };
-    } // constructor
-  } // class GameBoard
-
-  class Ship {
-    constructor(name, shipLength) {
-      Ship.numberOfShips++;
-      this.name = name;
-      this.health = shipLength;
-
-      this.resultOfGettingHit = function () {
-        this.health--;
-
-        this.health > 0
-          ? tellPlayer("Coronavirus resistente")
-          : (tellPlayer("Encontraste el " + this.name),
-            Ship.numberOfShips--,
-            Ship.numberOfShips > 0
-              ? tellPlayer(
-                  "Estas cerca de encontrar la vacuna  " + this.name + "!",
-                  4000
-                )
-              : won()); //: tellPlayer("You won!")) ;
-      };
-    }
-  }
-
-  function findPossiblePoints(rowOrColumn, shipLength) {
-    let counter = 0;
-    const maxLengthAtEachPoint = inReverseOrder(
-      inReverseOrder(rowOrColumn).map((value) =>
-        value === "" ? (counter = 0) : (counter += 1)
-      )
-    );
-
-    const collectAllPossiblePlaces = maxLengthAtEachPoint.reduce(function (
-      total,
-      value,
-      index
-    ) {
-      if (value >= shipLength) {
-        total.push(rowOrColumn[index]);
-      }
-      return total;
-    },
-    []);
-
-    return collectAllPossiblePlaces;
-  }
-
-  function fire() {
-    const typedInGuess = document
-      .getElementById("user-input")
-      .value.toLowerCase();
-    const guessInCorrectFormat = typedInGuess.match(/[a-j]{1}-?[0-9]{1}$/i);
-    document.getElementById("user-input").value = "";
-
-    if (guessInCorrectFormat === null) {
-      tellPlayer("Ingresa una coordenada validad, A0-J9");
-    } else {
-      let firstLetterToNumber = guessInCorrectFormat[0].charCodeAt(0) - 97;
-      let guessParsedToCellId = parseInt(
-        firstLetterToNumber + guessInCorrectFormat[0].slice(-1)
-      );
-
-      if (pastGuesses.includes(guessParsedToCellId)) {
-        tellPlayer("Ya has atacado esa cuadrícula, adivina de nuevo.");
-      } else {
-        pastGuesses.push(guessParsedToCellId);
-        gameBoard.checkIfShotHitsAnything(guessParsedToCellId);
-      }
-    }
-  }
-
-  function hit(location) {
-    document.getElementById(location).className = "hit";
-    numberOfHits++;
-    numberOfHits % 3 === 0
-      ? tellPlayer("Has encontrado otra vacuna")
-      : numberOfHits % 2 === 0
-      ? tellPlayer("Vacuna")
-      : tellPlayer("Estas cerca de la vacuna");
-  }
-
-  function miss(location) {
-    document.getElementById(location).className = "miss";
-    numberOfMisses++;
-    numberOfMisses % 3 === 0
-      ? tellPlayer("Sigues infectado, Busca la vacuna")
-      : numberOfMisses % 2 === 0
-      ? tellPlayer("¡¡ALERTA!! Virus esparcido")
-      : tellPlayer("Fallaste");
-  }
-
-  function won() {
-    document
-      .getElementById("fire-button")
-      .removeEventListener("click", fire);
-    document.getElementById("user-input").onkeypress = (event) =>
-      event.keyCode === 13 ? false : false;
-    document.getElementById("fire-button").id = "fire-button-disarmed";
-
-    tellPlayer("Sobreviviste en " + pastGuesses.length + " intentos.", 4000);
-    tellPlayer(
-      "Making your accuracy: " +
-        Math.round((numberOfHits / pastGuesses.length) * 100) +
-        " percent.",
-      7000
-    );
-    tellPlayer("Game Over...", 12000);
-    tellPlayer("Ahora apaga tu computadora y ve a buscar una vida.", 20000);
-  }
-  function tellPlayer(messageToPlayer, delay) {
-    let now = new Date();
-    let timeSinceLastMessage =
-      now - loggedMessageTimes[loggedMessageTimes.length - 1];
-    let timeOut = delay || 2000 - timeSinceLastMessage; // manual || default : if <= 0 doesn't delay
-    loggedMessageTimes.push(now);
-
-    setTimeout(function () {
-      document.getElementById("message").innerHTML = messageToPlayer;
-    }, timeOut);
-  }
-
-  const inReverseOrder = (array) => array.slice().reverse();
-
-  const randomNumber = (upperLimit) =>
-    Math.floor(Math.random() * (upperLimit + 1));
-
-  const chooseRandomFromArray = (array) =>
-    array[randomNumber(array.length - 1)];
-
-  const gameBoard = new GameBoard(100);
-
-  Ship.numberOfShips = 0;
+    class GameBoard {
+      constructor(cellNumber) {
+        this.cellNumber = [];
   
-  let fragatawil = new Ship("Fragata Will", 5);
-  let cbvictoria = new Ship("C.B Victoria", 4);
-  let pwromanian = new Ship("PW Romanian", 3);
-  let sscorvetta = new Ship("SS Corvetta", 3);
-  let destroyer = new Ship("Destroyer", 2);
-
-  let shipList = [fragatawil, cbvictoria, pwromanian, sscorvetta, destroyer];
-
-  for (ship of shipList) {
-    gameBoard.placeShip(ship);
-  }
-
-  document.getElementById("fire-button").addEventListener("click", fire);
-  document.getElementById("user-input").onkeypress = fireOnEnterKey;
-
-  function fireOnEnterKey(event) {
-    if (event.keyCode === 13) {
-      fire();
-      return false;
+        this.checkAvailableSpace = function (
+          isVertical,
+          startNumber,
+          shipLength
+        ) {
+          const freeSpaces = [];
+          let startCell = startNumber;
+          let endCell = 0;
+          let increase = 1;
+  
+          isVertical
+            ? ((endCell = startNumber + 91), (increase = 10))
+            : ((startCell = startNumber * 10), (endCell = startCell + 10));
+  
+          for (let i = startCell; i < endCell; i += increase) {
+            if (typeof this[i] === "undefined") {
+              freeSpaces.push(i);
+            } else {
+              freeSpaces.push("");
+            }
+          }
+          return findPossiblePoints(freeSpaces, shipLength);
+        };
+  
+        this.placeShip = function (ship) {
+          const isVertical = randomNumber(1);
+          const increase = isVertical * 9 + 1;
+          const endCell = ship.health * increase;
+          let keepGoing = true;
+  
+          while (keepGoing) {
+            let startIndex = randomNumber(9);
+            var possibilities = this.checkAvailableSpace(
+              isVertical,
+              startIndex,
+              ship.health
+            );
+            possibilities.length == 0
+              ? (keepGoing = true)
+              : (keepGoing = false);
+          }
+          const shipStart = chooseRandomFromArray(possibilities);
+          console.log(possibilities);
+          console.log("placing " + ship.name + " at " + shipStart);
+  
+          for (let i = 0; i < endCell; i += increase) {
+            gameBoard[shipStart + i] = ship;
+          }
+        };
+  
+        this.checkIfShotHitsAnything = (newGuess) => {
+          this[parseInt(newGuess)]
+            ? (hit(newGuess), this[newGuess].resultOfGettingHit())
+            : miss(newGuess); // calls ship.resultOfGettingHit() or tells "missed"
+        };
+      } // constructor
+    } // class GameBoard
+  
+    class Ship {
+      constructor(name, shipLength) {
+        Ship.numberOfShips++;
+        this.name = name;
+        this.health = shipLength;
+  
+        this.resultOfGettingHit = function () {
+          this.health--;
+  
+          this.health > 0
+            ? tellPlayer("Coronavirus resistente")
+            : (tellPlayer("Encontraste el " + this.name),
+              Ship.numberOfShips--,
+              Ship.numberOfShips > 0
+                ? tellPlayer(
+                    "Estas cerca de encontrar la vacuna  " + this.name + "!",
+                    4000
+                  )
+                : won()); //: tellPlayer("You won!")) ;
+        };
+      }
     }
-  }
+  
+    function findPossiblePoints(rowOrColumn, shipLength) {
+      let counter = 0;
+      const maxLengthAtEachPoint = inReverseOrder(
+        inReverseOrder(rowOrColumn).map((value) =>
+          value === "" ? (counter = 0) : (counter += 1)
+        )
+      );
+  
+      const collectAllPossiblePlaces = maxLengthAtEachPoint.reduce(function (
+        total,
+        value,
+        index
+      ) {
+        if (value >= shipLength) {
+          total.push(rowOrColumn[index]);
+        }
+        return total;
+      },
+      []);
+  
+      return collectAllPossiblePlaces;
+    }
+  
+    function fire() {
+         const typedInGuess = document
+        .getElementById("user-input")
+        .value.toLowerCase();
+      const guessInCorrectFormat = typedInGuess.match(/[a-j]{1}-?[0-9]{1}$/i);
+      document.getElementById("user-input").value = "";
+  
+      if (guessInCorrectFormat === null) {
+        tellPlayer("Ingresa una coordenada validad, A0-J9");
+      } else {
+        let firstLetterToNumber = guessInCorrectFormat[0].charCodeAt(0) - 97;
+        let guessParsedToCellId = parseInt(
+          firstLetterToNumber + guessInCorrectFormat[0].slice(-1)
+        );
+  
+        if (pastGuesses.includes(guessParsedToCellId)) {
+          tellPlayer("Ya has atacado esa cuadrícula, adivina de nuevo.");
+        } else {
+          pastGuesses.push(guessParsedToCellId);
+          gameBoard.checkIfShotHitsAnything(guessParsedToCellId);
+        }
+      }
+    }
+  
+    function hit(location) {
+      document.getElementById(location).className = "hit";
+      numberOfHits++;
+      numberOfHits % 3 === 0
+        ? tellPlayer("Has encontrado otra vacuna")
+        : numberOfHits % 2 === 0
+        ? tellPlayer("Vacuna")
+        : tellPlayer("Estas cerca de la vacuna");
+    }
+  
+    function miss(location) {
+      contador--;
+    document.getElementById("contador").textContent = contador;
+    con(contador);  
+  
+      document.getElementById(location).className = "miss";
+      numberOfMisses++;
+      numberOfMisses % 3 === 0
+        ? tellPlayer("Sigues infectado, Busca la vacuna")
+        : numberOfMisses % 2 === 0
+        ? tellPlayer("¡¡ALERTA!! Virus esparcido")
+        : tellPlayer("Fallaste");
+    }
+  
+    function won() {
+      document
+        .getElementById("fire-button")
+        .removeEventListener("click", fire);
+      document.getElementById("user-input").onkeypress = (event) =>
+        event.keyCode === 13 ? false : false;
+      document.getElementById("fire-button").id = "fire-button-disarmed";
+  
+      tellPlayer("Sobreviviste en " + pastGuesses.length + " intentos.", 4000);
+      tellPlayer(
+        "Making your accuracy: " +
+          Math.round((numberOfHits / pastGuesses.length) * 100) +
+          " percent.",
+        7000
+      );
+      tellPlayer("Game Over...", 12000);
+      tellPlayer("Ahora apaga tu computadora y ve a buscar una vida.", 20000);
+    }
+    function tellPlayer(messageToPlayer, delay) {
+      let now = new Date();
+      let timeSinceLastMessage =
+        now - loggedMessageTimes[loggedMessageTimes.length - 1];
+      let timeOut = delay || 2000 - timeSinceLastMessage; // manual || default : if <= 0 doesn't delay
+      loggedMessageTimes.push(now);
+  
+      setTimeout(function () {
+        document.getElementById("message").innerHTML = messageToPlayer;
+      }, timeOut);
+      a = a -1;
+    }
+  
+    const inReverseOrder = (array) => array.slice().reverse();
+  
+    const randomNumber = (upperLimit) =>
+      Math.floor(Math.random() * (upperLimit + 1));
+  
+    const chooseRandomFromArray = (array) =>
+      array[randomNumber(array.length - 1)];
+  
+    const gameBoard = new GameBoard(100);
+  
+    Ship.numberOfShips = 0;
+    
+    let fragatawil = new Ship("Fragata Will", 5);
+    let cbvictoria = new Ship("C.B Victoria", 4);
+    let pwromanian = new Ship("PW Romanian", 3);
+    let sscorvetta = new Ship("SS Corvetta", 3);
+    let destroyer = new Ship("Destroyer", 2);
+  
+    let shipList = [fragatawil, cbvictoria, pwromanian, sscorvetta, destroyer];
+  
+    for (ship of shipList) {
+      gameBoard.placeShip(ship);
+    }
+  
+    document.getElementById("fire-button").addEventListener("click", fire);
+    document.getElementById("user-input").onkeypress = fireOnEnterKey;
+  
+    function fireOnEnterKey(event) {
+      if (event.keyCode === 13) {
+        fire();
+        return false;
+      }
+    }
+  
+   
+  
+
+
+
+
 }
 
 function usa(){
+  var contador = 100;
+  function con(c){
+
+    if(c < 97){
+      document.getElementById("ms").textContent = "El virus te ha derrotado, ya es inútil buscar la cura";
+    } 
+  }
   let pastGuesses = [];
       let numberOfHits = 0;
       let numberOfMisses = 0;
@@ -370,6 +399,9 @@ function usa(){
       }
 
       function miss(location) {
+        contador--;
+    document.getElementById("contador").textContent = contador;
+    con(contador);  
         document.getElementById(location).className = "miss";
         numberOfMisses++;
         numberOfMisses % 3 === 0
@@ -443,6 +475,13 @@ function usa(){
 }
 
 function belice(){
+  var contador = 100;
+  function con(c){
+
+    if(c < 97){
+      document.getElementById("ms").textContent = "El virus te ha derrotado, ya es inútil buscar la cura";
+    } 
+  }
   let pastGuesses = [];
   let numberOfHits = 0;
   let numberOfMisses = 0;
@@ -591,6 +630,9 @@ function belice(){
   }
 
   function miss(location) {
+    contador--;
+    document.getElementById("contador").textContent = contador;
+    con(contador);
     document.getElementById(location).className = "miss";
     numberOfMisses++;
     numberOfMisses % 3 === 0
@@ -666,6 +708,13 @@ function belice(){
 }
 
 function colombia(){
+  var contador = 100;
+  function con(c){
+
+    if(c < 97){
+      document.getElementById("ms").textContent = "El virus te ha derrotado, ya es inútil buscar la cura";
+    } 
+  }
   let pastGuesses = [];
   let numberOfHits = 0;
   let numberOfMisses = 0;
@@ -814,6 +863,9 @@ function colombia(){
   }
 
   function miss(location) {
+    contador--;
+    document.getElementById("contador").textContent = contador;
+    con(contador);
     document.getElementById(location).className = "miss";
     numberOfMisses++;
     numberOfMisses % 3 === 0
@@ -886,6 +938,13 @@ function colombia(){
 }
 
 function alemania(){
+  var contador = 100;
+  function con(c){
+
+    if(c < 97){
+      document.getElementById("ms").textContent = "El virus te ha derrotado, ya es inútil buscar la cura";
+    } 
+  }
   let pastGuesses = [];
   let numberOfHits = 0;
   let numberOfMisses = 0;
@@ -1034,6 +1093,9 @@ function alemania(){
   }
 
   function miss(location) {
+    contador--;
+    document.getElementById("contador").textContent = contador;
+    con(contador);
     document.getElementById(location).className = "miss";
     numberOfMisses++;
     numberOfMisses % 3 === 0
@@ -1109,6 +1171,13 @@ function alemania(){
 }
 
 function china(){
+  var contador = 100;
+  function con(c){
+
+    if(c < 97){
+      document.getElementById("ms").textContent = "El virus te ha derrotado, ya es inútil buscar la cura";
+    } 
+  }
   let pastGuesses = [];
   let numberOfHits = 0;
   let numberOfMisses = 0;
@@ -1257,6 +1326,9 @@ function china(){
   }
 
   function miss(location) {
+    contador--;
+    document.getElementById("contador").textContent = contador;
+    con(contador);
     document.getElementById(location).className = "miss";
     numberOfMisses++;
     numberOfMisses % 3 === 0
@@ -1329,6 +1401,13 @@ function china(){
 }
 
 function australia(){
+  var contador = 100;
+  function con(c){
+
+    if(c < 97){
+      document.getElementById("ms").textContent = "El virus te ha derrotado, ya es inútil buscar la cura";
+    } 
+  }
   let pastGuesses = [];
   let numberOfHits = 0;
   let numberOfMisses = 0;
@@ -1477,6 +1556,9 @@ function australia(){
   }
 
   function miss(location) {
+    contador--;
+    document.getElementById("contador").textContent = contador;
+    con(contador);
     document.getElementById(location).className = "miss";
     numberOfMisses++;
     numberOfMisses % 3 === 0
